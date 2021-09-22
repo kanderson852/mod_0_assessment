6469df5402ece0ad9445a24f367b1bd0751dfcda
Kelly Anderson <kean9528@colorado.edu>
2021-09-22 17:32:01 -0600

    Model a dinosaur

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+Dinosaur = {
+  name: "Barney",
+  dinosaur_type: "T-Rex",
+  age_in_years: 7,
+  weight_in_lbs: 500,
+  purple: true,
+  big: true,
+  skills: ["acting","dancing","singing"],
+  favorite_food: ["lettuce", "burger"]
+}
