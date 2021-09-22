2c92affd70849f3d0e5e1adeb732ad9dc81afae8
Kelly Anderson <kean9528@colorado.edu>
2021-09-22 17:39:38 -0600

    Model a developer

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+Developer = {
+  name: "Kelly",
+  birthday: "July 14",
+  age_in_years: 25,
+  age_in_months: 302,
+  blonde_hair: true,
+  brunette_hair: false,
+  pets: ["Dog","Cat"],
+  cities_lived_in: ["Aurora", "Boulder", "Denver"]
+}
