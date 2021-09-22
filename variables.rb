e0bb461ba35090ae953f1456d800628f643972ae
Kelly Anderson <kean9528@colorado.edu>
2021-09-22 17:22:09 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "Finding Nemo"
+is_entertaining = true
+movie = "The Hobbit"
+num_4 = 4
+num_6 = 6
+product = num_4 * num_6
+favorite_foods = ["pears", "peaches", "strawberries"]
+favorite_foods.shift
+p movie.length
